214021094dfc90e5f707d103d819253dc7d2f3b1
ggomjae <ggomjae@naver.com>
2020-07-11 16:26:10 +0900

    new Person 2개를 하면 __proto__ 가 가리키는 것이 같다.

--- prototype.js
+++ prototype.js
@@ -10,12 +10,17 @@ function Person(name){
 }
 
 const tempObj = new Person('ggomjae')
+const temp2Obj = new Person('ggomjae2')
 
 function sayInfo(){
    console.log(o)
    console.log(o.__proto__)
    console.log(o.__proto__.__proto__)
    console.log(tempObj)
+   console.log('---------------------\n')
+   console.log(tempObj.__proto__ === temp2Obj.__proto__)
+   console.log(temp2Obj.name = 'ggomjae3')
+   console.log(temp2Obj.name + " " + tempObj.name)
 }
 
 
